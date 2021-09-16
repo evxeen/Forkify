cf701de8f030bbebfbfd1b594cb966d5e3521794
evxeen <evxeen@gmail.com>
2021-09-16 15:36:25 +0300

    refactor: refactoring for project architecture

---
+++ src/js/model.js
@@ -0,0 +1,30 @@
+import 'regenerator-runtime/runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=5d383cab-1a41-48fc-839a-55c49a2b76dc`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
