9cab26e385264bd1c276219ab88008f631740e3c
evxeen <evxeen@gmail.com>
2021-09-17 10:11:20 +0300

    feature: (controller.js) publisher-subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,9 +28,7 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
-
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
