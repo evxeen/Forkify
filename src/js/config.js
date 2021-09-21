b955717dcdfe73bfa65b80dfe10c0f44313a101e
evxeen <evxeen@gmail.com>
2021-09-21 23:03:14 +0300

    feature: render new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '4e3a13bb-52bf-494b-a21a-33b0c83260e8';
+export const MODAL_CLOSE_SEC = 2.5;
